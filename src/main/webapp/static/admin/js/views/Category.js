0d65f4fdf92cc723c8ed69e09dc9ae3444366bd8
Vinh Nguyen <thvinh2001@gmail.com>
2022-05-29 16:28:14 +0700

    Modal category event, edit category

--- src/main/webapp/static/admin/js/views/Category.js
+++ src/main/webapp/static/admin/js/views/Category.js
@@ -11,26 +11,89 @@ export default class Category extends AbstractView{
         this.getHtml();
         this.getScript();
     }
-    updateTableCategory(start, end){
+
+    addRowListener(row,modal){
+        let editBtn = row.querySelector('td.edit');
+        editBtn.addEventListener('click',()=>{
+            if(modal.classList.contains("active")==false){
+                modal.style.opacity = 1;
+                modal.classList.toggle("active");
+            }
+            modal.setAttribute('data-id',row.dataset.id);
+            modal.setAttribute('data-old_parent_id',row.dataset.parentid);
+            modal.querySelector('#editCateName').value = row.dataset.name;
+            modal.querySelector('#superCate').value = row.dataset.parentid;
+        })
+    }
+
+    addModalListener(modal){
+        modal.querySelectorAll('.modal-toggle').forEach(elem=>{
+            elem.addEventListener('click',(e)=>{
+                if (e.target.closest('#modal-btn')) {
+                    const newParentID = modal.querySelector('#superCate').value;
+                    if(modal.dataset.old_parent_id==0 &&newParentID!=0) {
+                        alert("Không thể sửa danh mục lớn");
+                    } else {
+                        let payLoad = {
+                            id: modal.dataset.id,
+                            name: document.querySelector('#editCateName').value,
+                            parent_id:  newParentID
+                        }
+                        this.updateCategory(payLoad);
+                    }
+                }
+                if(modal.classList.contains("active")){
+                    modal.style.opacity = 0;
+                    setTimeout(()=>{
+                        modal.classList.toggle("active");
+                    },300)
+                }
+            })
+        })
+    }
+
+    updateCategory(payLoad) { 
+        fetch(`http://localhost:8080/OrganicStore/api-category`,{
+            method : "PUT",
+            body : JSON.stringify(payLoad)
+        }).then(res=>res.json())
+        .then(data=>{
+            const rowEdit = document.querySelector(`tr[data-id="${payLoad.id}"]`);
+            rowEdit.dataset.name = data.name;
+            rowEdit.dataset.parent_id = data.parent_id;
+            rowEdit.querySelector('h5').textContent = data.name;
+            for(const [index,value] of Object.entries(this.categoryData)) {
+            
+                if(value.id == data.id) {
+                    this.categoryData[index] = data;
+                }
+            
+            }
+        })
+    }
+
+    updateTable(start, end){
         const arr = this.categoryData.slice(start, end);
             let html = arr.map(row => {
                 return `
-                            <tr>
-                                <td class="d-none" data-id="${row.id}"></td>
-                                <td > 
-                                    <h5>${row.name}</h5>
-                                </td>
-                                <td class="d-none" data-parenID="${row.parent_id}"></td>
-                                <td class="edit"><ion-icon name="create-outline" ></ion-icon></td>
-                            </tr>
-                        `
+                    <tr data-parentID="${row.parent_id}" data-id="${row.id}" data-name="${row.name}">
+                        <td class="d-none" ></td>
+                        <td > 
+                            <h5>${row.name}</h5>
+                        </td>
+                        <td class="d-none" ></td>
+                        <td class="edit"><ion-icon name="create-outline" ></ion-icon></td>
+                    </tr>
+                `
             }).join("");
         document.querySelector("tbody").innerHTML = html;
+        let modal = document.querySelector('.modal');
+        [...document.querySelector('tbody').children].forEach(e=> this.addRowListener(e,modal));
     }
-    loadSelectCustom() {
+    loadSelectCustom(selectTag) {
         let optionContainer = document.querySelector('.options-container');
         let superCategory  = this.categoryData.filter(e=>e.parent_id==0); 
-        let selectTag = document.querySelector('#selectCat');
+        // let selectTag = document.querySelector('#selectCat');
         selectTag.appendChild(this.elementFrom(`
             <option value="0">--- Danh mục lớn</option>
         `))
@@ -40,7 +103,6 @@ export default class Category extends AbstractView{
                 <option value="${e.id}">${e.name}</option>
             `))
         }
-
         // select filter table
         if(optionContainer.childElementCount==1){
             superCategory.forEach(e=>{
@@ -58,6 +120,7 @@ export default class Category extends AbstractView{
             superCategory.forEach(e=>select_add(e));
         }
     }
+
     getTotalItem(){ 
         return new Promise((resolve,reject)=>{
             console.time('t');
@@ -91,8 +154,9 @@ export default class Category extends AbstractView{
             
             initPagination(this.totalItem,this.itemPerPage);
             AddPaginationEvent(this.totalItem,this.itemPerPage);
-            this.updateTableCategory(0, this.itemPerPage);
-            this.loadSelectCustom();
+            this.updateTable(0, this.itemPerPage);
+            
+            [document.querySelector('#selectCat'),document.querySelector('#superCate')].forEach(e=>this.loadSelectCustom(e));
             this.mainElement.classList.remove("d-none");
         });
 
@@ -153,6 +217,8 @@ export default class Category extends AbstractView{
         
         </div>
         `;
+        this.mainElement.insertAdjacentHTML("beforeend",modalCategory);
+        this.addModalListener(this.mainElement.querySelector('.modal'));
         document.querySelector('.cardHeader').insertAdjacentHTML('beforeend',this.SelectCustom.elements.selectHtml);
     }
     getScript(){
@@ -161,12 +227,39 @@ export default class Category extends AbstractView{
                 page = Number(document.querySelector(".number.active").dataset.page),
                 start = (page - 1) * this.itemPerPage,
                 end = Math.min(start+ this.itemPerPage, this.totalItem);
-            this.updateTableCategory(start, end);
+            this.updateTable(start, end);
         })
            
     }
 }
 
+const modalCategory = `
+<div  class="modal ">
+    <div class="modal-container  category-modal ">
+        <div class="modal-close modal-toggle" >
+            <i class="fa-solid fa-xmark"></i>
+        </div>
+        <header class="modal-header">
+            <i class="modal-heading-icon fa-solid fa-face-laugh"></i>
+            Sửa danh mục
+        </header>
+        
+        <div class="modal-body ">
+            <label for="editCateName">Tên danh mục</label>
+            <input id="editCateName" name="name" type="text" class="modal-input" placeholder="Nhập tên danh mục" required>
+        </div>
+        <div class="modal-body ">
+            <label for="superCate">Chọn danh mục cha (Tùy chọn)</label>
+            <select id="superCate"  class="modal-input" ></select>
+        </div>
 
+        <div class="modal-footer" >
+            <button id="modal-btn" type="button" class="modal-toggle">
+                Lưu
+            </button>
+        </div>
+    </div>
+</div>
+`;
 // 
 
